17ac934f1ba5e54323a5cf9418449a9afafa6226
Artsy Biba <biba.artsiom@gmail.com>
2021-02-20 19:06:36 -0500

    adding colors for winners and losers

--- app.js
+++ app.js
@@ -12,17 +12,34 @@ p1Button.addEventListener('click', () => {
     if (p1Score.innerText < maxScore && p2Score.innerText < maxScore) {
         p1Score.innerText++
     };
+
+    if (p1Score.innerText == maxScore) {
+        p1Score.classList.add('winner');
+        p2Score.classList.add('loser');
+    }
 })
 
 p2Button.addEventListener('click', () => {
     if (p1Score.innerText < maxScore && p2Score.innerText < maxScore) {
         p2Score.innerText++
     };
+
+    if (p2Score.innerText == maxScore) {
+        p1Score.classList.add('loser');
+        p2Score.classList.add('winner');
+    }
 })
 
 resetButton.addEventListener('click', () => {
     p1Score.innerText = 0;
     p2Score.innerText = 0;
+
+    p1Score.classList == 'winner' 
+        ? p1Score.classList.remove('winner')
+        : p1Score.classList.remove('loser');
+    p2Score.classList == 'winner' 
+        ? p2Score.classList.remove('winner')
+        : p2Score.classList.remove('loser');
 })
 
 playTo.addEventListener('change', () => {
